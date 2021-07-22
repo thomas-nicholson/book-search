5172b36181d864d06fd710528fa7320f016c659f
Thomas Nicholson <24470776+thomas-nicholson@users.noreply.github.com>
2021-07-23 00:14:03 +1000

    Added Apollo-server-express with middleware to server

--- server/server.js
+++ server/server.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const path = require('path');
+const { ApolloServer } = require('apollo-server-express');
+
 const db = require('./config/connection');
-const routes = require('./routes');
+//const routes = require('./routes');
+const { typeDefs, resolvers } = require('./schemas');
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.applyMiddleware({ app });
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -14,7 +24,11 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+//app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
