471fda4c32c2245b5c022c01de5d716575725636
Thomas Nicholson <24470776+thomas-nicholson@users.noreply.github.com>
2021-07-23 20:40:32 +1000

    Added Resolvers for Query me and Mutation login

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,32 @@
 const { User, Book } = require('../models');
 
-const resolvers = {
+// import sign token function from auth
+const { signToken } = require('../utils/auth');
 
+const resolvers = {
+    Query: {
+        me: async (parent, { _id }) => {
+            return User.findOne({_id});
+        }
+    },
+    Mutation: {
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email: email });
+            if (!user) {
+                return res.status(400).json({ message: "Can't find this user" });
+            }
+
+            const correctPw = await user.isCorrectPassword(body.password);
+
+            if (!correctPw) {
+                return res.status(400).json({ message: 'Wrong password!' });
+            }
+
+            const token = signToken(user);
+            return { token, user };
+        },
+
+    }
 };
 
 module.exports = resolvers;
